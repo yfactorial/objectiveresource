88c48c938f35db1d90405db363eb623bae542181
vickeryj <vickeryj@j-macbook.westell.com>
2009-01-28 17:16:47 -0500

    [#21] create lib only zip archive as well

--- package_zip.sh
+++ package_zip.sh
@@ -3,3 +3,7 @@ git pull
 git submodule init
 git submodule update
 zip -r objective_resource . -x build/\* .git\* .git/\*
+cd Classes/lib
+zip -r objective_resource_lib_only . -x .git\* .git/\*
+mv objective_resource_lib_only.zip ../../
+cd ../../
