a83588dccf5c7226c88e334ec6658a5a3ce9e564
vickeryj <vickeryj@j-macbook.local>
2009-02-02 10:52:00 -0500

    [#21] work on packaging

--- package_zip.sh
+++ package_zip.sh
@@ -2,8 +2,16 @@
 git pull
 git submodule init
 git submodule update
-zip -r objective_resource . -x build/\* .git\* .git/\*
-cd Classes/lib
-zip -r objective_resource_lib_only . -x objective_support/.git/\* objective_support/.git\*
-mv objective_resource_lib_only.zip ../../
-cd ../../
+ORES_DIR=`pwd`
+cd ../
+zip -r objective_resource $ORES_DIR -x $ORES_DIR/build/\* $ORES_DIR/.git\* $ORES_DIR/.git/\*
+mv objective_resource.zip $ORES_DIR
+cd $ORES_DIR/Classes/
+zip -r objective_resource_lib_only lib -x lib/objective_support/\* lib/objective_support/
+mv objective_resource_lib_only.zip lib/objective_support/Classes
+cd lib/objective_support/Classes
+cp -R lib objective_support
+mv objective_support lib/objective_support
+zip -r objective_resource_lib_only lib/objective_support
+rm -rf lib/objective_support
+mv objective_resource_lib_only.zip ../../../../
