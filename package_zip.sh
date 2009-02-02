e763a9435b71f5a185df6d47c1443f2cfda7ed9a
vickeryj <vickeryj@j-macbook.local>
2009-02-02 08:58:57 -0500

    [#21] update package script

--- package_zip.sh
+++ package_zip.sh
@@ -4,6 +4,6 @@ git submodule init
 git submodule update
 zip -r objective_resource . -x build/\* .git\* .git/\*
 cd Classes/lib
-zip -r objective_resource_lib_only . -x .git\* .git/\*
+zip -r objective_resource_lib_only . -x objective_support/.git/\* objective_support/.git\*
 mv objective_resource_lib_only.zip ../../
 cd ../../
