d4da28a915bb7a86af3a8d5c3ac7cf7e15b61bbd
vickeryj <vickeryj@j-macbook.local>
2009-02-02 11:04:05 -0500

    [#21] fix directory name

--- package_zip.sh
+++ package_zip.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
+
+#update
 git pull
 git submodule init
 git submodule update
+
+#zip everything
 ORES_DIR=`pwd`
-cd ../
-zip -r objective_resource $ORES_DIR -x $ORES_DIR/build/\* $ORES_DIR/.git\* $ORES_DIR/.git/\*
+
+mkdir -p ../ores_zip_temp_dir/objective_resource
+cd ../ores_zip_temp_dir/objective_resource
+cp -R $ORES_DIR/* .
+cd ..
+zip -r objective_resource objective_resource -x objective_resource/build/\* objective_resource/.git\* objective_resource/.git/\* objective_resource/Classes/lib/objective_support/.git\/* objective_resource/Classes/lib/objective_support/.git\*
 mv objective_resource.zip $ORES_DIR
-cd $ORES_DIR/Classes/
-zip -r objective_resource_lib_only lib -x lib/objective_support/\* lib/objective_support/
-mv objective_resource_lib_only.zip lib/objective_support/Classes
-cd lib/objective_support/Classes
-cp -R lib objective_support
-mv objective_support lib/objective_support
-zip -r objective_resource_lib_only lib/objective_support
-rm -rf lib/objective_support
-mv objective_resource_lib_only.zip ../../../../
+cd $ORES_DIR
+rm -rf ../ores_zip_temp_dir
+
+#zip lib only
+#cd $ORES_DIR/Classes/
+#zip -r objective_resource_lib_only lib -x lib/objective_support/\* lib/objective_support/
+#mv objective_resource_lib_only.zip lib/objective_support/Classes
+#cd lib/objective_support/Classes
+#cp -R lib objective_support
+#mv objective_support lib/objective_support
+#zip -r objective_resource_lib_only lib/objective_support
+#rm -rf lib/objective_support
+#mv objective_resource_lib_only.zip ../../../../
